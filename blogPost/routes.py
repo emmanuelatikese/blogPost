2e1b06a860632967130e4e8938876c05f33497ed
emmanuelatikese <emmanuelatikese14@gmail.com>
2023-09-06 09:12:13 +0000

    add

--- blogPost/routes.py
+++ blogPost/routes.py
@@ -1,6 +1,15 @@
 from blogPost import app
-from flask import render_template
+from flask import render_template, url_for
 
 @app.route('/')
 @app.route('/home')
 def homepage(): return render_template('home.html')
+
+@app.route('/profile')
+def profilepage(): return render_template('profile.html')
+
+@app.route('/createBlog')
+def createpage():return render_template('createBlog.html')
+
+@app.route('/viewBlog')
+def viewBlogpage(): return render_template('viewBlog.html')
